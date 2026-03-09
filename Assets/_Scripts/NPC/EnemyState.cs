581488f97e03ba71d344f9ad71cfaf81d8220067
Danny Simsek <simsek95@yahoo.de>
2026-03-09 21:45:49 +0100

    Random Enemy Turn on Idle + Added Enemy Sight

--- Assets/_Scripts/NPC/EnemyState.cs
+++ Assets/_Scripts/NPC/EnemyState.cs
@@ -24,58 +24,7 @@ public class EnemyState : MonoBehaviour
         return;
     }
 
-    protected bool TargetIsInLineOfSight(Transform _target)
-    {
-        if (_target == null) return false;
-
-        Vector2 direction = (Vector2)_target.position - (Vector2)transform.position;
-        float distance = direction.magnitude;
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, owner.obstacleLayer);
-
-        if (hit.collider == null)
-        {
-            Debug.DrawRay(transform.position, direction.normalized * distance, Color.green, 10);
-            Debug.Log("LOS YES");
-            return true;
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, direction.normalized * hit.distance, Color.red, 10);
-            Debug.Log("LOS NO");
-            return false;
-        }
-    }
-
-    public bool CanSeeTarget(Transform _target)
-    {
-        if (_target == null) return false;
-
-        // Debug.Log("Angle: " + TargetIsInFrontAngle(_target));
-        return Vector3.Distance(transform.position, _target.position) < owner.detectionRange && TargetIsInLineOfSight(_target) && TargetIsInFrontAngle(_target);
-    }
-
-    protected bool TargetIsInFrontAngle(Transform _target)
-    {
-       
-        Vector2 directionToTarget = (_target.position - transform.position).normalized;
-
-        Vector2 lookDirection = transform.up;
-
-        float angleToTarget = Vector2.Angle(lookDirection, directionToTarget);
-        bool canSee = angleToTarget < (owner.viewAngle * 0.5f);
-
-       
-        return canSee;
-    }
-
-    protected bool IsInTargetRange(Transform _target)
-    {
-        if (_target == null) return false;
-
-
-        return Vector3.Distance(transform.position, _target.position) < owner.attackRange;
-    }
+   
 
     protected void SetSpeed()
     {
